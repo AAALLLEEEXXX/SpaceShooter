b964ffd18a3a5099f94490de36135ee84e070d90
a.gavrikov <a.gavrikov@gamingsense.ru>
2021-01-31 11:49:39 +0300

    Стрельба. Папка с врагами

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _damage = 5.0f;
+
+    [SerializeField]
+    private float _range = 50.0f;
+
+    private Camera _camera;
+
+    private void Awake()
     {
-        
+        _camera = GetComponent<Camera>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var point = new Vector3(_camera.pixelWidth / 2, _camera.pixelHeight / 2, 0);
+            var ray = _camera.ScreenPointToRay(point);
+
+            if (Physics.Raycast(ray, out var hit, _range))
+            {
+                Debug.Log(hit.transform.name);
+            }
+        }
     }
 }
